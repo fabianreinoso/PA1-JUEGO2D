30dfbfd1ba126187d2fe6fcfd707a469e924df04
Fabian Ramirez Reinoso <fabian.ramirez.r@tecsup.edu.pe>
2026-09-09 22:27:49 -0500

    Añadiendo script de salto

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,12 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
     private float move; 
 
+    public float jumpforce = 4;
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float groundRadius = 0.1f;
+    public LayerMask groundLayer;
+
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
@@ -15,6 +21,20 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        move = Input.GetAxisRaw("Horizontal");
+        rb2d.linearVelocity = new Vector2(move*speed, rb2d.linearVelocity.y);
+
+        if(move != 0)
+            transform.localScale = new Vector3(Mathf.Sign(move),1,1);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb2d.linearVelocity = new Vector2(rb2d.linearVelocity.x, jumpforce);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
     }
 }
