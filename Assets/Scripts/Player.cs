3f9faa97373117b7204ddab63d930d749c6e6d28
Fabian Ramirez Reinoso <fabian.ramirez.r@tecsup.edu.pe>
2026-09-09 23:24:26 -0500

    Añadiendo script de monedas

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,9 +13,12 @@ public class NewMonoBehaviourScript : MonoBehaviour
     public float groundRadius = 0.1f;
     public LayerMask groundLayer;
 
+    private Animator animator;
+
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -31,10 +34,25 @@ public class NewMonoBehaviourScript : MonoBehaviour
         {
             rb2d.linearVelocity = new Vector2(rb2d.linearVelocity.x, jumpforce);
         }
+
+        animator.SetFloat("Speed", Mathf.Abs(move));
+        animator.SetFloat("VerticalVelocity", rb2d.linearVelocity.y);
+        animator.SetBool("IsGrounded", isGrounded);                                                     
+
     }
 
     private void FixedUpdate()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.transform.CompareTag("Coin")){
+            Destroy(collision.gameObject);
+        }
+    }
+
+
+
 }
